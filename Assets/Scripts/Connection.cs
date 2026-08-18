0247c80192d06f328407514042c804b209029e54
Lee JaeHyun <myleje@gmail.com>
2026-08-18 13:59:22 +0900

    .

--- Assets/Scripts/Connection.cs
+++ Assets/Scripts/Connection.cs
@@ -27,7 +27,17 @@ public class Connection : MonoBehaviour
         // REST API 전송
         UnityWebRequest request = UnityWebRequest.Post(url, json, "application/json");
         Debug.Log("[접속] Ollama 서버에 요청 전송 중 ...");
-        
-        
+
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            // 응답 출력
+            Debug.Log($"[응답] 연결 성공!\n응답 메시지: {request.downloadHandler.text}");
+        }
+        else
+        {
+            Debug.Log($"[연결 실패] {request.error}");
+        }
     }
 }
