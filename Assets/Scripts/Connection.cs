986bd6ddc0b784dc51e41ce38ada2c3c24d8124e
Lee JaeHyun <myleje@gmail.com>
2026-08-18 13:40:50 +0900

    feat: Add server settings to Connection class

--- Assets/Scripts/Connection.cs
+++ Assets/Scripts/Connection.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 
 public class Connection : MonoBehaviour
 {
-
+    [Header("Ollama Sever Settings")]
+    [SerializeField] private string serverURL = "http://localhost:11434";
+    [SerializeField] private string modelName = "gemma4:e2b";
 }
