a69b6ad7a042f520b1eb444fde055c1fc60d9e47
Lee JaeHyun <myleje@gmail.com>
2026-08-19 10:15:40 +0900

    .

--- Assets/02_Scripts/ChatManager.cs
+++ Assets/02_Scripts/ChatManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
@@ -9,4 +10,10 @@ public class ChatManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _npcDialogText;
     [SerializeField] private TMP_InputField _userInputField;
     [SerializeField] private Button _sendButton;
+    
+    private OllamaClient _ollamaClient;
+    private NpcPersona _npcPersona;
+    
+    // 대화 히스토리 관리를 위한 컬렉션
+    public List<OllamaMessage> _history = new();
 }
