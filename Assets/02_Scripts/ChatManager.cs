2576f538829da71e6f41d3266a8e9c378795d3fc
Lee JaeHyun <myleje@gmail.com>
2026-08-19 10:24:47 +0900

    .

--- Assets/02_Scripts/ChatManager.cs
+++ Assets/02_Scripts/ChatManager.cs
@@ -16,4 +16,23 @@ public class ChatManager : MonoBehaviour
     
     // 대화 히스토리 관리를 위한 컬렉션
     public List<OllamaMessage> _history = new();
+    
+    // 페르소나 로딩
+    private void LoadPersona()
+    {
+        TextAsset json = Resources.Load<TextAsset>("WizardPersona");
+        // 역직렬화 (json => 객체)
+        _npcPersona = JsonUtility.FromJson<NpcPersona>(json.text);
+        
+        _history.Add(new OllamaMessage
+        {
+            role = "system",
+            content = $"당신의 이름은 {_npcPersona.name} 입니다. " +
+                      $"성격 : {_npcPersona.personality} " +
+                      $"배경 : {_npcPersona.background} " +
+                      $"말투 : {_npcPersona.speechStyle} " +
+                      "반드시 캐릭터 성격을 유지하고, 한글로 답하세요."
+        });
+    }
+    
 }
